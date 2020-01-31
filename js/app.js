3e3d0007342a3d3c6a63a3e0fbbd3497017e226e
Benjamin Carpenter <bcarpenter@ucvts.org>
2020-01-31 13:18:30 -0500

    suiha

---
+++ js/app.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+  document.getElementByID("1") = markXY;
+  document.getElementById("reset-button") = reset;
+}
+
+function markXY = function(clicked_id) {
+  alert(clicked_id)
+}
