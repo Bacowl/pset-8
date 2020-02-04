da45ba8d631506fbc286b26e78a60bf814f4ddbe
Benjamin Carpenter <bcarpenter@ucvts.org>
2020-02-04 13:22:47 -0500

    progress

--- js/app.js
+++ js/app.js
@@ -1,8 +1,79 @@
-window.onload = function() {
-  document.getElementByID("1") = markXY;
-  document.getElementById("reset-button") = reset;
+///////////////////// CONSTANTS /////////////////////////////////////
+const winningConditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+];
+const markedIndex = [];
+
+///////////////////// APP STATE (VARIABLES) /////////////////////////
+let turn;
+let board;
+
+let win;
+///////////////////// CACHED ELEMENT REFERENCES /////////////////////
+const squares = Array.from(document.querySelectorAll("#board div"));
+const message = document.querySelector("h2");
+///////////////////// EVENT LISTENERS ///////////////////////////////
+window.onload = init
+document.getElementById("board").onclick = takeTurn;
+document.getElementById("reset-button").onclick = init;
+///////////////////// FUNCTIONS /////////////////////////////////////
+
+
+
+function init() {
+  board = [
+    "", "", "",
+    "", "", "",
+    "", "", ""
+  ];
+  turn = "X";
+  win = null;
+  render();
 }
 
-function markXY = function(clicked_id) {
-  alert(clicked_id)
+function getWinner() {
+  let winner = null;
+
+  winningConditions.forEach(function(condition, index) {
+    if (
+      board[condition[0]] &&
+      board[condition[0]] === board[condition[1]] &&
+      board[condition[1]] === board[condition[2]]
+    ) {
+      winner = board[condition[0]];
+    }
+  });
+
+  return winner ? winner : board.includes("") ? null : "T";
+}
+function takeTurn(e) {
+  if (!win) {
+    let index = squares.findIndex(function(square) {
+      return square === e.target;
+    });
+
+    if (board[index] === "") {
+      markedIndex[index.length] = index
+      alert(markedIndex)
+      board[index] = turn;
+      turn = turn === "X" ? "O" : "X";
+      win = getWinner();
+
+      render();
+    }
+  }
+}
+function render() {
+  board.forEach(function(mark, index) {
+    squares[index].textContent = mark;    // writes an X or an O on board
+  });
+  alert(board)
+  message.textContent = `Turn: ${turn}`;
 }
